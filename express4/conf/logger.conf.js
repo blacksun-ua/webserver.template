0981519138fcac9c7193d5cc7b6ac13a3b3913c4
blacksun-ua <dsp_one@mail.bar>
2014-08-12 11:43:31 +0300

    update logger config

--- express4/conf/logger.conf.js
+++ express4/conf/logger.conf.js
@@ -1,41 +1,44 @@
-var name = 'template';
+var name        = 'template'
+  , maxLogSize  = 1024*1024*10
+  , backups     = 3
+  ;
 
 module.exports = {
-	appenders : [ {
-		type : 'file',
-		filename : '/var/log/' + name + '/main.log',
-		category : name + '.main',
-		maxLogSize : 1204800,
-		backups : 3
-	}, {
-		type : 'file',
-		filename : '/var/log/' + name + '/db.log',
-		category : name + '.db',
-		maxLogSize : 1204800,
-		backups : 3
-	}, {
-		type : 'file',
-		filename : '/var/log/' + name + '/server.in.log',
-		category : name + '.server.in',
-		maxLogSize : 1204800,
-		backups : 3
-	}, {
-		type : 'file',
-		filename : '/var/log/' + name + '/server.in.out.ipc.log',
-		category : name + '.server.in.out',
-		maxLogSize : 1204800,
-		backups : 3
-	}, {
-        type : 'file',
-        filename : '/var/log/' + name + '/client.req.log',
-        category : name + '.client.req',
-        maxLogSize : 1204800,
-        backups : 3
+    appenders : [ {
+        type        : 'file',
+        filename    : '/var/log/' + name + '/main.log',
+        category    : name + '.main',
+        maxLogSize  : maxLogSize,
+        backups     : backups
     }, {
-        type : 'file',
-        filename : '/var/log/' + name + '/client.req.rsp.log',
-        category : name + '.client.req.rsp',
-        maxLogSize : 1204800,
-        backups : 3
+        type        : 'file',
+        filename    : '/var/log/' + name + '/db.log',
+        category    : name + '.db',
+        maxLogSize  : maxLogSize,
+        backups     : backups
+    }, {
+        type        : 'file',
+        filename    : '/var/log/' + name + '/server.in.log',
+        category    : name + '.server.in',
+        maxLogSize  : maxLogSize,
+        backups     : backups
+    }, {
+        type        : 'file',
+        filename    : '/var/log/' + name + '/server.in.out.ipc.log',
+        category    : name + '.server.in.out',
+        maxLogSize  : maxLogSize,
+        backups     : backups
+    }, {
+        type        : 'file',
+        filename    : '/var/log/' + name + '/client.req.log',
+        category    : name + '.client.req',
+        maxLogSize  : maxLogSize,
+        backups     : backups
+    }, {
+        type        : 'file',
+        filename    : '/var/log/' + name + '/client.req.rsp.log',
+        category    : name + '.client.req.rsp',
+        maxLogSize  : maxLogSize,
+        backups     : backups
     } ]
 };
